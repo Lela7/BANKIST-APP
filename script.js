64ef4935d5c82e8680d3041ce21fd25f4c26c672
Danijela Bodnar <danijelabodnar@hotmail.com>
2024-01-09 16:11:56 +0100

    Adding movements to our app using forEach and insertAdjacementHTML

--- script.js
+++ script.js
@@ -61,6 +61,30 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  //to empty container and start adding only new elements:
+  containerMovements.innerHTML = '';
+  //.textContent=0; //similar things
+  //////////////////////////////////
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          
+          <div class="movements__value">${mov}</div>
+    </div>`;
+    //to insert this HTML, we use this method insertAdjacentHTML:
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+    //we use 'beforeend' when we want inverted elements
+  });
+};
+displayMovements(account1.movements);
+
+//checking what we've created F12:
+//console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
