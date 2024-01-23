a952a8460987b17eece4ddae52fc0494f42d6982
Danijela Bodnar <danijelabodnar@hotmail.com>
2024-01-23 10:42:49 +0100

    Changing movements' dates into TODAY, YESTERDAY, X DAYS AGO

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2024-01-20T14:43:26.374Z',
+    '2024-01-19T18:49:59.371Z',
+    '2024-01-22T12:01:20.894Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -38,7 +38,7 @@ const account2 = {
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
+    '2024-01-20T14:43:26.374Z',
     '2024-01-19T18:49:59.371Z',
     '2024-01-22T12:01:20.894Z',
   ],
@@ -82,10 +82,15 @@ const formatMovementDate = function (date) {
   const daysPassed = calcDaysPassed(new Date(), date);
   console.log(daysPassed);
 
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 };
 const displayMovements = function (acc, sort = false) {
   //to empty container and start adding only new elements:
@@ -302,13 +307,3 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
-const future = new Date(2037, 10, 19, 15, 23);
-console.log(Number(future));
-console.log(+future);
-
-//Calculating days between two dates:
-const calcDaysPassed = (date1, date2) =>
-  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
-
-const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
-console.log(days1);
